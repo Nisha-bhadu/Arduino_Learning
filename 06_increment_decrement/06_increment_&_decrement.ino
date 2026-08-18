6fda03f452a8611ccbe2aae1288bfd9c2bc386bc
Nisha Bhadu <nisha9499206673@gmail.com>
2026-08-19 01:03:29 +0530

    Add files via upload

---
+++ 06_increment_decrement/06_increment_&_decrement.ino
@@ -0,0 +1,15 @@
+int x = 10;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  x++;
+  x--;
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(x);
+
+
+}
