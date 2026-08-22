0e26beace3e5af5f4a5ccad49a2f703847fdedac
Nisha Bhadu <nisha9499206673@gmail.com>
2026-08-22 20:19:54 +0530

    Add files via upload

---
+++ 08_comparison_operator/08_comparison_operator.ino
@@ -0,0 +1,17 @@
+int x = 20 , y = 30;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(x>y);
+   Serial.println(x<y);
+    Serial.println(x>=y);
+     Serial.println(x<=y);
+      Serial.println(x==y);
+       Serial.println(x!=y);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
